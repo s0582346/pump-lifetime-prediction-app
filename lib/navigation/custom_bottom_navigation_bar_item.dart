e130daf112644f6477edd01c2f46c7a308202b2c
s0582346 <s0582346@htw-berlin.de>
2025-01-12 17:54:43 +0100

    added bottom navigation

---
+++ lib/navigation/custom_bottom_navigation_bar_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBarItem extends BottomNavigationBarItem {
+  
+  CustomBottomNavigationBarItem({
+    required String assetPath,
+    Color inactiveColor = Colors.grey,
+    Color activeColor = const Color(0xFF007167),
+    double inactiveWidth = 30.0,
+    double inactiveHeight = 30.0,
+    double activeWidth = 35.0,
+    double activeHeight = 35.0,
+    String label = '',
+  }) : super(
+          icon: Image.asset(
+            assetPath,
+            color: inactiveColor,
+            width: inactiveWidth,
+            height: inactiveHeight,
+          ),
+          activeIcon: Image.asset(
+            assetPath,
+            color: activeColor,
+            width: activeWidth,
+            height: activeHeight,
+          ),
+          label: label,
+        );
+}
