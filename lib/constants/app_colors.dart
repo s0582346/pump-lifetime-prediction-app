35c775bff79513b3018bf70c84d5d7292f622371
s0582346 <s0582346@htw-berlin.de>
2025-04-27 19:12:13 +0200

    clean up

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,5 +4,4 @@ class AppColors {
   static const Color primaryColor = Color(0xFF007167);
   static const Color greyColor = Color.fromARGB(255, 199, 198, 198);
   static const Color errorMessageColor = Color.fromARGB(255, 173, 53, 45);
-  // Add more colors as needed
 }
